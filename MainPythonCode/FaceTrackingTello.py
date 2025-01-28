854178b58160e7d7453c166ee56a4fe287036e04
Gargi Nisal <gargpan1@gmail.com>
2025-01-28 14:36:51 +0000

    FACE TRACKING

---
+++ MainPythonCode/FaceTrackingTello.py
@@ -0,0 +1,27 @@
+from utils import *
+import cv2
+
+w,h= 360, 240
+pid=[0.5,0.5,0] #kp,kd, and ki
+myDrone = initializeTello()
+pError=0
+startCounter = 0 #For no flight, set it as 1, for flight, set to 0qq Qqqq
+
+while True:
+    #FLIGHT
+    if startCounter == 0:
+        myDrone.takeoff()
+        startCounter=1
+
+    img = telloGetFrame(myDrone, (w,h))
+    img,info = findFace(img)
+    #GIVES INFORMATION ABOUT THE CENTRE POINT
+    print(info[0][0])
+
+    #Using PID controller
+    pError= trackFace(myDrone, info, w, pid, pError)
+
+    cv2.imshow('Image', img)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        myDrone.land()
+        break
---
+++ MainPythonCode/utils.py
@@ -0,0 +1,79 @@
+import cv2
+from djitellopy import Tello
+import numpy as np
+
+
+def initializeTello():
+    myDrone = Tello()
+    myDrone.connect()
+    myDrone.for_back_velocity= 0
+    myDrone.left_right_velocity= 0
+    myDrone.up_down_velocity= 0
+    myDrone.yaw_velocity= 0
+    myDrone.speed = 0
+    myDrone.direction = 1
+    myDrone.streamoff()
+    myDrone.streamon()
+    return myDrone
+
+def telloGetFrame(myDrone, w=360, h=240):
+    myFrame = myDrone.get_frame_read()
+    myFrame = myFrame.frame
+    img = cv2.resize(myFrame, w,h)
+    return img
+
+def findFace(img):
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray,1.2,4)
+
+    #CODE TO PREVENT TRACKING MUTLIPLE FACES
+    myFaceListC= []
+    myFaceListArea = []
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        cx = x + w//2
+        cy = y + h//2
+        area = w*h
+        myFaceListArea.append(area)
+        myFaceListC.append([cx,cy])
+
+    if len(myFaceListArea)!= 0:
+        #Find index of largest area, i.e. the closest face to the screen
+        i = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListC[i], myFaceListArea[i]]
+
+    else:
+        return img, [[0,0], 0]
+    
+def trackFace(myDrone, info, w, pid, pError):
+    #PID Controllers
+    error=info[0][0] - w//2
+    speed = pid[0]*error + pid[1]*(error-pError)
+    speed= int(np.clip(speed, -100, 100))#Make sure the speed stays in the boundaries
+
+    if info[0][0]!=0:
+        myDrone.yaw_velocity=speed
+    else: #Keeps moving in a direction until a finds a face
+        myDrone.for_back_velocity= 0
+        myDrone.left_right_velocity= 0
+        myDrone.yaw_velocity= 30 #Continuously rotates
+
+        if myDrone.get_height() >= 175:  # Max height to avoid over-lift
+            myDrone.direction = -1
+        elif myDrone.get_height() <= 150:  # Min height to avoid crashing
+            myDrone.direction = 1
+        
+        myDrone.up_down_velocity = myDrone.direction * 20  # Move up or down at a constant speed
+        
+        error=0
+    
+    if myDrone.send_rc_control:
+        myDrone.send_rc_control(myDrone.left_right_velocity, myDrone.for_back_velocity,
+                                myDrone.up_down_velocity, myDrone.yaw_velocity)
+    return error
+
+
+
+
