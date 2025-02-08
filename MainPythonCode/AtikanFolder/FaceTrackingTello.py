e0f24613dc60c6174700a07ed23366153f33ae6a
iSpecimen <iSpecimenCreatorAcc@gmail.com>
2025-02-08 13:05:18 +0000

    most up to date code

--- MainPythonCode/AtikanFolder/FaceTrackingTello.py
+++ MainPythonCode/AtikanFolder/FaceTrackingTello.py
@@ -4,56 +4,13 @@ import cv2
 w,h= 360, 240
 pid=[0.5,0.5,0] #kp,kd, and ki
 myDrone = initializeTello()
-pError=0
+pError, pError_ud= 0, 0
+
 startCounter = 0 #For no flight, set it as 1, for flight, set to 0
 track= True
 trackbody= False
 
-def get_keyboard_input():
-    lr, fb, ud, yaw = 0, 0, 0, 0  # Left/Right, Forward/Backward, Up/Down, Rotate
-    speed = 80  # Speed of movement
-    keyinput = False
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == -1:
-        return None
-    if key == ord('q'):  # Quit and land
-        myDrone.land()
-        return "quit"
-
-    if key == ord('w'):
-        fb = speed
-        keyinput=True
-    elif key == ord('s'):
-        fb = -speed
-        keyinput=True
-
-    if key == ord('j'):  # Left (Left Arrow)
-        lr = -speed
-        keyinput=True
-    elif key == ord('l'):  # Right (Right Arrow)
-        lr = speed
-        keyinput=True
-
-    if key == ord('i'):  # Up
-        ud = speed
-        keyinput=True
-    elif key == ord('k'):  # Down
-        ud = -speed
-        keyinput=True
-
-    if key == ord('a'):  # Rotate Left
-        yaw = -speed
-        keyinput=True
-    elif key == ord('d'):  # Rotate Right
-        yaw = speed
-        keyinput=True
-    if keyinput:
-        myDrone.send_rc_control(lr, fb, ud, yaw)
-    else:
-        return None
-    
-    return None
 
 while True:
     #FLIGHT
@@ -72,17 +29,17 @@ while True:
 
     #Using PID controller
     if track:
-        pError= trackFB(myDrone, info, w, pid, pError, area)
+        pError, pError_ud= trackFB(myDrone, info, w, h, pid, pError, pError_ud, area)
     # if detectThumbsUp(img):
     #     myDrone.rotate_clockwise(180)
 
 
     cv2.imshow('Drone View', img)
 
-    # Get manual keyboard input
-    control_command = get_keyboard_input()
-    if control_command == "quit":
-        break  # Exit loop if 'q' is pressed
+    # # Get manual keyboard input
+    # control_command = get_keyboard_input()
+    # if control_command == "quit":
+    #     break  # Exit loop if 'q' is pressed
 
     # CONTINGENCY STUFF FOR PRESENTATION 
 myDrone.land()
--- MainPythonCode/AtikanFolder/utils.py
+++ MainPythonCode/AtikanFolder/utils.py
@@ -22,6 +22,51 @@ def telloGetFrame(myDrone, w=360, h=240):
     img = cv2.resize(myFrame, w,h)
     return img
     
+def get_keyboard_input(myDrone):
+    lr, fb, ud, yaw = 0, 0, 0, 0  # Left/Right, Forward/Backward, Up/Down, Rotate
+    speed = 80  # Speed of movement
+    keyinput = False
+
+    key = cv2.waitKey(1) & 0xFF
+ 
+    if key == ord('q'):  # Quit and land
+        myDrone.land()
+        return "quit"
+
+    if key == ord('w'):
+        fb = speed
+        keyinput=True
+    elif key == ord('s'):
+        fb = -speed
+        keyinput=True
+
+    if key == ord('j'):  # Left (Left Arrow)
+        lr = -speed
+        keyinput=True
+    elif key == ord('l'):  # Right (Right Arrow)
+        lr = speed
+        keyinput=True
+
+    if key == ord('i'):  # Up
+        ud = speed
+        keyinput=True
+    elif key == ord('k'):  # Down
+        ud = -speed
+        keyinput=True
+
+    if key == ord('a'):  # Rotate Left
+        yaw = -speed
+        keyinput=True
+    elif key == ord('d'):  # Rotate Right
+        yaw = speed
+        keyinput=True
+    if keyinput:
+        myDrone.send_rc_control(lr, fb, ud, yaw)
+        keyinput=False
+    else:
+        keyinput= False
+        return "No Input"
+    return None
 
 def findFace(img):
     faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -72,17 +117,24 @@ def findFace(img):
 #     else:
 #         return img, [[0,0], 0], area
     
-def trackFB(myDrone, info, w, pid, pError, area):
-    #PID Controllers
+def trackFB(myDrone, info, w, h, pd, pError, pError_ud, area):
+    foundface=False
+    #PD Controllers (Proportional Derivative Controller)
     error=info[0][0] - w//2
-    speed = pid[0]*error + pid[1]*(error-pError) 
-    speed= int(np.clip(speed, -100, 100))#Make sure the speed stays in the boundaries
+    speed = pd[0]*error + pd[1]*(error-pError) #pError is previous error, calculate change in error to know the rate of change of error.
+    speed= int(np.clip(speed, -100, 100)) #Make sure the speed stays in the boundaries
+
+    # --- Up/Down Movement (PD Controller) ---
+    error_ud = info[0][1] - h//2 
+    speed_ud = pd[0] * error_ud + pd[1] * (error_ud - pError_ud)
+    speed_ud = int(np.clip(speed_ud, -90, 90))  # Limit altitude speed
 
     if info[0][0]!=0:
-        print("FOUND TRACK!")
+        print("FOUND TRACK! (Ensure Safe distance movement)")
+        foundface=True
         myDrone.yaw_velocity=speed
-        myDrone.up_down_velocity=0
-        if (area > 3600):
+        myDrone.up_down_velocity=-speed_ud
+        if (area > 3300):
             myDrone.for_back_velocity = -25
         elif (area <2700):
             myDrone.for_back_velocity = 25
@@ -102,9 +154,9 @@ def trackFB(myDrone, info, w, pid, pError, area):
         error=0
     
     if myDrone.send_rc_control:
-        myDrone.send_rc_control(myDrone.left_right_velocity, myDrone.for_back_velocity,
-                                myDrone.up_down_velocity, myDrone.yaw_velocity)
-    return error
+        if get_keyboard_input(myDrone) == "No Input":
+            myDrone.send_rc_control(myDrone.left_right_velocity, myDrone.for_back_velocity, myDrone.up_down_velocity, myDrone.yaw_velocity)
+    return error, error_ud
 
     
 # def detectThumbsUp(img):
